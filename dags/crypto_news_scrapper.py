93d6a78ec06b5c9557b061bd8784289419134b02
alice <aliciabasilo.ab@gmail.com>
2025-07-13 00:50:54 +0000

    fixies

--- dags/crypto_news_scrapper.py
+++ dags/crypto_news_scrapper.py
@@ -48,6 +48,7 @@ with DAG(
         execution_timeout=timedelta(minutes=5),
         startup_timeout_seconds=600,
         get_logs=True,
+        cmds=["sh", "-c"],
         arguments=["Xvfb :99 -screen 0 1024x768x24 & export DISPLAY=:99 && python scraper.py"],
        
     )
