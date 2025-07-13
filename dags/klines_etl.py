902868427b61da5cef6f25ca4cf4e6cf1123367c
alice <aliciabasilo.ab@gmail.com>
2025-07-13 19:08:23 +0000

    used entrypoint from docker

--- dags/klines_etl.py
+++ dags/klines_etl.py
@@ -48,6 +48,6 @@ with DAG(
     execution_timeout=timedelta(minutes=15),
     startup_timeout_seconds=900,
     get_logs=True,
-    cmds=["python"],
-    arguments=["-m" ,"src.backfill"],
+    #cmds=["python"],
+    #arguments=["-m" ,"src.backfill"],
 )
