b29c97430cce8866b08413b6ce1c2e25e481531b
alice <aliciabasilo.ab@gmail.com>
2025-07-22 03:35:55 +0000

    incresead time out

--- dags/crypto_news_scrapper.py
+++ dags/crypto_news_scrapper.py
@@ -45,7 +45,7 @@ with DAG(
     image="registry-docker-registry.registry.svc.cluster.local:5000/crypto_news_scrapper:latest",
     secrets=[env_secret],
     is_delete_operator_pod=True,
-    execution_timeout=timedelta(minutes=10),
+    execution_timeout=timedelta(minutes=50),
     startup_timeout_seconds=900,
     get_logs=True,
     cmds=["/bin/bash", "-c"],
