12461095cbfe3c7ad34c8802121a17939d09567d
alice <aliciabasilo.ab@gmail.com>
2026-07-19 22:10:52 -0600

    Give 4h ETL mapping a fresh task identity

--- dags/crypto-forecast-pipeline-4h.py
+++ dags/crypto-forecast-pipeline-4h.py
@@ -106,7 +106,7 @@ with DAG(
     with TaskGroup("etl") as etl:
         etl_4h = (
             KubernetesPodOperator.partial(
-                task_id="etl_4h",
+                task_id="etl_4h_by_coin",
                 namespace="gatsbyt",
                 image="390402534126.dkr.ecr.us-east-1.amazonaws.com/klines-etl@sha256:0af2df904835fd3b4ebf019c4091897d8eb723d5da92958936b064a79e4a9f99",
                 secrets=[db_secret],
